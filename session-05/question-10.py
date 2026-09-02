1a2e60bd26ef485433fb3c215d451722f34a9b34
parsa999 <309002576+jtk999py@users.noreply.github.com>
2026-09-02 12:11:30 +0330

    Update question-10.py

--- session-05/question-10.py
+++ session-05/question-10.py
@@ -1,1 +1,23 @@
-'''برنامه ایی بنویسید که دو جمله از کاربر بگیر ید و مشخص کن ید چه کلماتی در هر دو جمله وجود دارند'''
+'''برنامه ایی بنویسید که دو جمله از کاربر بگیرد و مشخص کند  چه کلماتی در هر دو جمله وجود دارند'''
+
+#inputs
+
+x = input ('enter first sentence : ')
+y = input ('enter second sentence : ')
+
+#lists
+
+m = x.split()
+n = y.split()
+ze = []
+
+#functions
+
+for i in m:
+    if i in n and i not in ze:
+        ze.append(i)
+        
+        
+#output
+
+print(f'the simultaneous words are --> {ze}')
