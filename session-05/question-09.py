f101db4f0030f1cfb4fd8a8061d2265d3ea6bb5b
parsa999 <309002576+jtk999py@users.noreply.github.com>
2026-09-02 11:55:51 +0330

    06/11

--- session-05/question-09.py
+++ session-05/question-09.py
@@ -5,3 +5,31 @@ Wrong username or password
 Atempts remaining: 2
 اگر درست بود پیام زیر را بدهد
 Login successful'''
+
+# mafroozat
+
+username = 'jtk999'           #username farzi
+password = 'fanavari.co.THR'  #pass farzi
+
+#shomarande ha
+
+attemtscounter = 0
+
+#functions and conditional statements
+
+while attemtscounter < 3 :
+    
+    mainusername = input ('username ra vared konid : ')
+    mainpassword = input ('password ra vared konid : ')
+    
+    if mainusername == username and password == mainpassword:
+        
+        print('login succsesful')
+        print('wellcome !')
+        break
+    
+    else:
+        attemtscounter = attemtscounter + 1
+        y = 3 - attemtscounter
+        print(f'Wrong username or password  2')
+        print(f'remaining attemts --> {y}')
