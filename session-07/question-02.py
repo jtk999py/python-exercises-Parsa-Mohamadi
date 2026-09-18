51124defb2139bbc86026da1760eaf7759473da3
parsa999 <309002576+jtk999py@users.noreply.github.com>
2026-09-18 10:53:25 +0330

    01 to 05

--- session-07/question-02.py
+++ session-07/question-02.py
@@ -9,47 +9,17 @@ process_order("Ali","Laptop","Mouse","Keyboard",discount=10,tax=9,shipping=20000
 اگر discount یا tax ارسال نشد، مقدار پیش‌فرض داشته 
 باشد.'''
 
-
-text = 'parsa'
-s = text[::-1]
-print(s)
-
-
-
-
-
-
-
-
-
+def procces_order(customer,*products,**options):
+    discount = options.get('discount',0)
+    tax = options.get('tax',9)
+    shiping = options.get('shipping',0)
+    price = 1000000
+    discount_amount = price*discount/100
+    price_after_discount = price - discount_amount
+    tax_amount = price_after_discount * tax / 100
+    final_price=price_after_discount+tax_amount+shiping
+    dict_result ={'customer':customer,'products':list(products),'discount':discount,'tax':tax,'shipping':shiping,'final price':final_price}
+    return dict_result
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(procces_order('ali','laptop','iphone 13','airpod',discount = 10 , tax = 20 , shipping = 0))
