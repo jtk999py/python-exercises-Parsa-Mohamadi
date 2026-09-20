793f2ab61240322c70351f71c44a63b86f74c854
parsa999 <309002576+jtk999py@users.noreply.github.com>
2026-09-20 16:54:12 +0330

    01 to 05

--- session-07/question-03.py
+++ session-07/question-03.py
@@ -14,37 +14,60 @@ Number of Transactions
 بیشترین برداشت
 فعال‌ترین کاربر
 '''
+def analyze_transactions(transactions):
+    result = {}
 
+    for transaction in transactions:
+        user_name, transaction_type, amount = transaction
 
+        if user_name not in result:
+            result[user_name] = {
+                'deposits': 0,
+                'withdrawals': 0,
+                'balance_change': 0,
+                'transactions': 0
+            }
 
+        if transaction_type == 'deposit':
+            result[user_name]['deposits'] += amount
+            result[user_name]['balance_change'] += amount
 
+        elif transaction_type == 'withdraw':
+            result[user_name]['withdrawals'] += amount
+            result[user_name]['balance_change'] -= amount
 
+        result[user_name]['transactions'] += 1
 
+    highest_deposit = 0
+    highest_withdrawal = 0
+    most_active = 0
 
+    highest_deposit_user = ""
+    highest_withdrawal_user = ""
+    most_active_user = ""
 
+    for user in result:
+        if result[user]["deposits"] > highest_deposit:
+            highest_deposit = result[user]["deposits"]
+            highest_deposit_user = user
 
+        if result[user]["withdrawals"] > highest_withdrawal:
+            highest_withdrawal = result[user]["withdrawals"]
+            highest_withdrawal_user = user
 
+        if result[user]["transactions"] > most_active:
+            most_active = result[user]["transactions"]
+            most_active_user = user
+        
+        return result,highest_deposit_user,highest_withdrawal_user,most_active_user
+    
+    
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(analyze_transactions(transactions=[("Ali", "deposit", 5000000),("Ali", "withdraw", 1000000),("Sara", "deposit", 8000000),("Ali", "withdraw", 500000),("Sara", "withdraw", 2000000),("Reza", "deposit", 10000000)]))
+        
+        
+        
+        
+        
+        
+        
--- session-07/question-04.py
+++ session-07/question-04.py
@@ -18,33 +18,103 @@ check_balance()
 generate_fraud_report()
 در نهایت گزارشی از تراکنش‌ها را در تابع آخر برگردانید.'''
 
+transactions = [
+    ("Ali", "deposit", 50000000, 10),
+    ("Ali", "withdraw", 2000000, 11),
+    ("Ali", "withdraw", 3000000, 12),
+    ("Ali", "withdraw", 4000000, 13),
+    ("Ali", "withdraw", 5000000, 14),
+    ("Ali", "withdraw", 6000000, 15),
+    ("Sara", "deposit", 50000000, 20),
+    ("Sara", "withdraw", 60000000, 21),
+    ("Reza", "deposit", 150000000, 30)
+]
 
 
+def check_large_transaction(transaction):
+    username, transaction_type, amount, time = transaction
+
+    if amount > 100000000:
+        return True
+
+    return False
 
 
+def check_repeated_withdrawals(transactions, index):
+    username = transactions[index][0]
+
+    if transactions[index][1] != "withdraw":
+        return False
+
+    count = 1
+    i = index - 1
+
+    while i >= 0 and transactions[i][0] == username and transactions[i][1] == "withdraw":
+        count += 1
+        i -= 1
+
+    i = index + 1
+
+    while i < len(transactions) and transactions[i][0] == username and transactions[i][1] == "withdraw":
+        count += 1
+        i += 1
+
+    if count > 3:
+        return True
+
+    return False
 
 
+def check_balance(transactions, index):
+    username = transactions[index][0]
+    balance = 0
+
+    for i in range(index + 1):
+        if transactions[i][0] == username:
+
+            if transactions[i][1] == "deposit":
+                balance += transactions[i][2]
+
+            elif transactions[i][1] == "withdraw":
+                balance -= transactions[i][2]
+
+    if balance < 0:
+        return True
+
+    return False
 
 
+def generate_fraud_report(transactions):
+    report = []
+
+    for i in range(len(transactions)):
+
+        transaction = transactions[i]
+        reasons = []
+
+        if check_large_transaction(transaction):
+            reasons.append("large transaction")
+
+        if check_repeated_withdrawals(transactions, i):
+            reasons.append("repeated withdrawals")
+
+        if check_balance(transactions, i):
+            reasons.append("insufficient balance")
+
+        if len(reasons) > 0:
+            report.append({
+                "transaction": transaction,
+                "reasons": reasons
+            })
+
+    return report
 
 
+def detect_fraud(transactions):
+    return generate_fraud_report(transactions)
 
 
+result = detect_fraud(transactions)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for item in result:
+    print(item)
